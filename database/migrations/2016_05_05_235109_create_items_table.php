3ce173b58f2a6cedc7000bcf0fbb8e44fdd76e3f
Jorge Gonzales Castillo <joryes1894@gmail.com>
2016-05-05 19:43:48 -0500

    Migraciones de entries and items

---
+++ database/migrations/2016_05_05_235109_create_items_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('product_id')->unsigned();
+            $table->foreign('product_id')->references('id')->on('products');
+            $table->string('series');
+            $table->enum('state', ['low', 'sold', 'rented', 'available', 'packaging']);
+            $table->integer('package_id')->unsigned()->nullable();
+            $table->foreign('package_id')->references('id')->on('packages');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('items');
+    }
+}
---
+++ database/migrations/2016_05_06_000527_create_entries_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEntriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('entries', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('provider_id')->unsigned();
+            $table->foreign('provider_id')->references('id')->on('providers');
+            $table->enum('type', ['local', 'foreign']);
+            $table->string('comment');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('entries');
+    }
+}
