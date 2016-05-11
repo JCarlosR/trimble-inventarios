c1476e25ba5e7bf010893b83a86d32f2bf6d2f74
Juan Carlos <juancagb.17@gmail.com>
2016-05-10 23:05:33 -0500

    Fix the f*cking Soles bug

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -5,9 +5,9 @@
 | Model Factories
 |--------------------------------------------------------------------------
 |
-| Here you may define all of your exemplar factories. Model factories give
+| Here you may define all of your model factories. Model factories give
 | you a convenient way to create models for testing and seeding your
-| database. Just tell the factory how a default exemplar should look.
+| database. Just tell the factory how a default model should look.
 |
 */
 
