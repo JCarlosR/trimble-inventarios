63aa294f4114d26cd9fc40f486a6671d5f587de9
Jorge Gonzales Castillo <joryes1894@gmail.com>
2016-05-31 13:25:45 -0500

    Avance Interfaz Package

--- resources/views/package/create.blade.php
+++ resources/views/package/create.blade.php
@@ -83,28 +83,47 @@
                                         <input type="text" name="nombre" class="form-control">
                                     </div>
                                 </div>
-
-
                                 <div class="col-md-6 ">
-                                    <label class="col-md-3" for="descripcion">Descripción</label>
+                                    <label class="control-label col-md-3" for="descripcion">Descripción: </label>
                                     <div class="input-group col-md-9">
-                                        <textarea name="descripcion" rows="2" class="form-control"></textarea>
+                                        <textarea style="resize: none"  name="descripcion" rows="2" class="form-control"></textarea>
                                     </div>
                                 </div>
                             </div>
-
                         </div>
-
+                        {{--<div class="form-group">--}}
+                            {{--<div class="row">--}}
+                                {{--<div class="col-md-3">--}}
+                                    {{--<label for="Ubicacion" class="control-label" >Seleccionar local</label>--}}
+                                    {{--<select name="locales" id="locales" class="form-control"></select>--}}
+                                {{--</div>--}}
+                                {{--<div class="col-md-3">--}}
+                                    {{--<label for="Ubicacion" class="control-label" >Seleccionar anaquel</label>--}}
+                                    {{--<select name="locales" id="locales" class="form-control"></select>--}}
+                                {{--</div>--}}
+                                {{--<div class="col-md-3">--}}
+                                    {{--<label for="Ubicacion" class="control-label" >Seleccionar nivel</label>--}}
+                                    {{--<select name="locales" id="locales" class="form-control"></select>--}}
+                                {{--</div>--}}
+                                {{--<div class="col-md-3">--}}
+                                    {{--<label for="Ubicacion" class="control-label" >Seleccionar caja</label>--}}
+                                    {{--<select name="locales" id="locales" class="form-control"></select>--}}
+                                {{--</div>--}}
+                            {{--</div>--}}
+                        {{--</div>--}}
                         <div class="form-group">
-                            <label class="control-label col-md-3 col-sm-3 col-xs-12" for="cliente">Producto: </label>
-                            <div class="input-group col-md-6 col-sm-6 col-xs-12">
-                                <input type="text" id="producto" class="typeahead form-control">
+                            <div class="row">
+                                <div class="col-md-6 col-md-offset-2">
+                                    <label class="control-label col-md-3" for="cliente">Producto: </label>
+                                    <div class="input-group col-md-9">
+                                        <input type="text" id="producto" class="typeahead form-control">
+                                    </div>
+                                </div>
+
+                                <div class="col-md-2 ">
+                                    <button type="submit" class="btn btn-primary">Agregar</button>
+                                </div>
                             </div>
-
-                        </div>
-
-                        <div class="col-md-4 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary btn-block">Agregar</button>
                         </div>
 
                         <div class="col-md-12 col-sm-12 col-xs-12">
@@ -114,34 +133,24 @@
                                     <table class="table table-hover">
                                         <thead>
                                         <tr>
-                                            <th>#</th>
                                             <th>Producto</th>
                                             <th>Nombre</th>
-                                            <th>Precio</th>
                                             <th>Serie</th>
-                                            <th>Marca</th>
-                                            <th>Modelo</th>
-                                            <th>NumParte</th>
-                                            <th>Color</th>
-                                            <th>Categoría</th>
-                                            <th>SubCategoría</th>
                                             <th>Observación</th>
+                                            <th>Acción</th>
                                         </tr>
                                         </thead>
                                         <tbody>
                                         <tr>
-                                            <th scope="row">1</th>
                                             <td>00001</td>
                                             <td>Producto X</td>
-                                            <td>120.00</td>
-                                            <td></td>
                                             <td>ZBC</td>
-                                            <td>XYZ</td>
-                                            <td>2</td>
-                                            <td>Azul</td>
-                                            <td>A</td>
-                                            <td>AB</td>
                                             <td>Observación</td>
+                                            <td>
+                                                <button type="button" class="btn btn-danger" title="Eliminar">
+                                                    Quitar
+                                                </button>
+                                            </td>
                                         </tr>
                                         </tbody>
                                     </table>
@@ -150,9 +159,9 @@
                             </div>
                         </div>
 
-                        <div class=" col-md-6 btn-group col-md-offset-5">
-                            <button type="submit" class="btn btn-success btn-lg">Registrar</button>
-                            <button type="reset" class="btn btn-danger btn-lg">Cancelar</button>
+                        <div class=" col-md-6 col-md-offset-5">
+                            <button type="submit" class="btn btn-success">Registrar</button>
+                            <a href="{{ url('paquete') }}" type="reset" class="btn btn-danger">Cancelar</a>
                         </div>
 
 
@@ -165,68 +174,67 @@
 
         <div class="modal fade" id="myModal" role="dialog">
             <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                        <h4 class="modal-title">Productos asociados</h4>
+                    </div>
 
+                    <div class="modal-body">
+                        <table class="table table-hover">
+                                        <thead>
+                                        <tr>
+                                            <th>#</th>
+                                            <th>Producto</th>
+                                            <th>Nombre</th>
+                                            <th>Precio</th>
+                                            <th>Serie</th>
+                                            <th>Marca</th>
+                                            <th>Modelo</th>
+                                            <th>NumParte</th>
+                                        </tr>
+                                        </thead>
+                                        <tbody>
+                                        <tr>
+                                            <th scope="row">1</th>
+                                            <td>00001</td>
+                                            <td>Producto X</td>
+                                            <td>12.80</td>
+                                            <td></td>
+                                            <td>ZBC</td>
+                                            <td>XYZ</td>
+                                            <td>2</td>
+                                        </tr>
 
-            <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal">&times;</button>
-                    <h4 class="modal-title">Productos asociados</h4>
-                </div>
+                                        <tr>
+                                            <th scope="row">1</th>
+                                            <td>00001</td>
+                                            <td>Producto X</td>
+                                            <td>14.60</td>
+                                            <td></td>
+                                            <td>ZBC</td>
+                                            <td>XYZ</td>
+                                            <td>2</td>
+                                        </tr>
 
-                <div class="modal-body">
-                    <table class="table table-hover">
-                                    <thead>
-                                    <tr>
-                                        <th>#</th>
-                                        <th>Producto</th>
-                                        <th>Nombre</th>
-                                        <th>Precio</th>
-                                        <th>Serie</th>
-                                        <th>Marca</th>
-                                        <th>Modelo</th>
-                                        <th>NumParte</th>
-                                    </tr>
-                                    </thead>
-                                    <tbody>
-                                    <tr>
-                                        <th scope="row">1</th>
-                                        <td>00001</td>
-                                        <td>Producto X</td>
-                                        <td>12.80</td>
-                                        <td></td>
-                                        <td>ZBC</td>
-                                        <td>XYZ</td>
-                                        <td>2</td>
-                                    </tr>
+                                        <tr>
+                                            <th scope="row">1</th>
+                                            <td>00001</td>
+                                            <td>Producto X</td>
+                                            <td>20.00</td>
+                                            <td>002</td>
+                                            <td>ZBC</td>
+                                            <td>XYZ</td>
+                                            <td>2</td>
+                                        </tr>
+                                        </tbody>
+                                    </table>
+                    </div>
 
-                                    <tr>
-                                        <th scope="row">1</th>
-                                        <td>00001</td>
-                                        <td>Producto X</td>
-                                        <td>14.60</td>
-                                        <td></td>
-                                        <td>ZBC</td>
-                                        <td>XYZ</td>
-                                        <td>2</td>
-                                    </tr>
-
-                                    <tr>
-                                        <th scope="row">1</th>
-                                        <td>00001</td>
-                                        <td>Producto X</td>
-                                        <td>20.00</td>
-                                        <td>002</td>
-                                        <td>ZBC</td>
-                                        <td>XYZ</td>
-                                        <td>2</td>
-                                    </tr>
-                                    </tbody>
-                                </table>
-                </div>
-
-                <div class="modal-footer btn-group">
-                    <button type="button" class="btn btn-primary btn-lg">Seleccionar</button>
-                    <button type="button" class="btn btn-danger btn-lg" data-dismiss="modal">Cancelar</button>
+                    <div class="modal-footer btn-group">
+                        <button type="button" class="btn btn-primary btn-lg">Seleccionar</button>
+                        <button type="button" class="btn btn-danger btn-lg" data-dismiss="modal">Cancelar</button>
+                    </div>
                 </div>
             </div>
         </div>
