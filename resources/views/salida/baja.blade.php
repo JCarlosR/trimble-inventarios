52443ebd17a18dc12445d6f2ba276b1525ebc8f2
Jorge Gonzales Castillo <joryes1894@gmail.com>
2016-06-01 18:20:22 -0500

    Salidas por bajas

--- resources/views/salida/baja.blade.php
+++ resources/views/salida/baja.blade.php
@@ -2,6 +2,59 @@
 
 @section('title', 'Salidas')
 
+@section('styles')
+    <style>
+        .typeahead,
+        .tt-query,
+        .tt-hint {
+            line-height: 30px;
+            -webkit-border-radius: 8px;
+            -moz-border-radius: 8px;
+            outline: none;
+        }
+        .typeahead:focus {
+            border: 1px solid #0097cf;
+        }
+        .tt-query {
+            -webkit-box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.075);
+            -moz-box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.075);
+            box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.075);
+        }
+        .tt-hint {
+            color: #bdbdbd;
+        }
+        .tt-menu {
+            margin: 12px 0;
+            padding: 8px 0;
+            background-color: #fff;
+            border: 1px solid rgba(0, 0, 0, 0.2);
+            -webkit-border-radius: 8px;
+            -moz-border-radius: 8px;
+            border-radius: 4px;
+            -webkit-box-shadow: 0 5px 10px rgba(0,0,0,.2);
+            -moz-box-shadow: 0 5px 10px rgba(0,0,0,.2);
+            box-shadow: 0 5px 10px rgba(0,0,0,.2);
+            color: #000;
+        }
+        .tt-suggestion {
+            padding: 3px 20px;
+            line-height: 24px;
+        }
+        .tt-suggestion:hover {
+            cursor: pointer;
+            color: #fff;
+            background-color: #0097cf;
+        }
+        .tt-suggestion.tt-cursor {
+            color: #fff;
+            background-color: #0097cf;
+        }
+        .tt-suggestion p {
+            margin: 0;
+        }
+    </style>
+@endsection
+
 @section('content')
     <div class="row">
         <div class="col-md-12 col-sm-12 col-xs-12">
@@ -28,16 +81,20 @@
                     </ul>
 
                     <div class="clearfix"></div>
-
+                    @if(session('error'))
+                        <div class="alert alert-danger" role="alert">
+                            <strong>Lo sentimos! </strong>Por favor revise los siguientes errores.
+                            <p>{{ session('error') }}</p>
+                        </div>
+                    @endif
                     <div class="x_content">
                         <br>
-                        <form id="demo-form2" data-parsley-validate="" class="form-horizontal form-label-left" novalidate="">
 
                             <div class="form-group">
-                                <label class="control-label col-md-3 col-sm-3 col-xs-12" for="date">Fecha
+                                <label class="control-label col-md-3" for="date">Fecha
                                 </label>
-                                <div class=" input-group col-md-6 col-sm-6 col-xs-12">
-                                    <input type="date" id="date" name="date" class="form-control col-md-7 col-xs-12">
+                                <div class=" input-group col-md-4">
+                                    <input type="date" id="date" name="date" class="form-control" value="{{ $currentDate }}">
 
                                 </div>
                             </div>
@@ -47,89 +104,88 @@
                                 </label>
 
                                 <div class="input-group col-md-3 col-md-offset-3" >
-                                    <input type="radio" id="last-name"  name="tipo" value="1" checked >Producto
-                                    <input type="radio" id="last-name"  name="tipo" value="0" >Paquete
+                                    <input type="radio" id="product"  name="tipo" value="Product" checked>Producto
+                                    <input type="radio" id="package"  name="tipo" value="Package">Paquete
                                 </div>
                             </div>
 
                             <div class="form-group">
-                                <label class="control-label col-md-3 col-sm-3 col-xs-12" for="cliente">Producto
+                                <label class="control-label col-md-3 col-sm-3 col-xs-12" for="cliente">Serie / Código único
                                 </label>
-                                <div class="input-group col-md-6 col-sm-6 col-xs-12">
-                                    <input type="text" class="form-control " placeholder="Buscar producto ...">
-                                    <span class="input-group-btn">
-                                      <button class="btn btn-default" data-toggle="modal" data-target="#myModal" type="button">Ver series</button>
-                                    </span>
+                                <div id="divProduct" class="input-group col-md-6 col-sm-6 col-xs-12">
+                                    <input type="text" name="codigoBaja" class="typeahead form-control" id="searchProduct" data-url="{{url('/')}}" placeholder="Buscar serie o código único ...">
+                                </div>
+                                <div id="divPackage" class="input-group col-md-6 col-sm-6 col-xs-12">
+                                    <input type="text" name="codigoBaja" class="typeahead form-control" id="searchPackage" data-url="{{url('/')}}" placeholder="Buscar serie o código único ...">
                                 </div>
 
-                            </div>
-
-                            <!-- Modal -->
-                            <div class="modal fade" id="myModal" role="dialog">
-                                <div class="modal-dialog">
-
-                                    <!-- Modal content-->
-                                    <div class="modal-content">
-                                        <div class="modal-header">
-                                            <button type="button" class="close" data-dismiss="modal">&times;</button>
-                                            <h4 class="modal-title">Modal Header</h4>
-                                        </div>
-                                        <div class="modal-body">
-                                            <table class="table table-hover table-condensed">
-                                                        <thead>
-                                                        <tr>
-                                                            <th>#</th>
-                                                            <th>Producto/Paquete</th>
-                                                            <th>Serie</th>
-                                                            <th>Acci�n</th>
-                                                        </tr>
-                                                        </thead>
-                                                        <tbody>
-                                                        <tr>
-                                                            <th scope="row">1</th>
-                                                            <td>1000001</td>
-                                                            <td>256314</td>
-                                                            <td>
-                                                                <button type="button" class="btn-success">Elegir</button>
-                                                            </td>
-                                                        </tr>
-                                                        <tr>
-                                                            <th scope="row">2</th>
-                                                            <td>1000002</td>
-                                                            <td>256314</td>
-                                                            <td>
-                                                                <button type="button" class="btn-success">Elegir</button>
-                                                            </td>
-                                                        </tr>
-                                                        <tr>
-                                                            <th scope="row">3</th>
-                                                            <td>1000003</td>
-                                                            <td>256314</td>
-                                                            <td>
-                                                                <button type="button" class="btn-success">Elegir</button>
-                                                            </td>
-                                                        </tr>
-                                                        </tbody>
-                                                    </table>
-                                        </div>
-                                        <div class="modal-footer">
-                                            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                                        </div>
-                                    </div>
-
-                                </div>
                             </div>
 
                             <div class="form-group">
                                 <div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-5">
-                                    <button type="submit" class="btn btn-primary">Dar de baja</button>
+                                    <button type="submit" class="btn btn-primary" id="btnBajar" >Dar de baja</button>
                                     <button type="submit" class="btn btn-danger">Cancelar</button>
                                 </div>
                             </div>
-                        </form>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+    <div id="modalDarBaja" class="modal fade in">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title">Dar de baja a producto</h4>
+                </div>
+                <form action="{{ url('/salida/baja') }}" method="POST">
+                    <div class="modal-body">
+
+                        <input type="hidden" name="_token" value="{{ csrf_token() }}" />
+                        <input type="hidden" name="tipo" />
+                        <div class="form-group">
+                            <label for="codigoDarBaja">¿Desea dar de baja el siguiente producto?</label>
+                            <input type="text" readonly class="form-control" id="codigoDarBaja" name="codigoDarBaja"/>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <div class="btn-group pull-left">
+                            <button class="btn btn-danger pull-left" data-dismiss="modal"><span class="glyphicon glyphicon-menu-up"></span> Cancelar</button>
+                        </div>
+                        <div class="btn-group pull-right">
+                            <button type="submit" class="btn btn-primary"><span class="glyphicon glyphicon-ok-circle" aria-hidden="true"></span> Aceptar</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@section('scripts')
+    <script src="{{ asset('js/typeahead.bundle.js') }}"></script>
+    <script src="{{ asset('js/output/baja.js') }}"></script>
+    <script>
+        var substringMatcher = function(strs) {
+            return function findMatches(q, cb) {
+                var matches, substringRegex;
+
+                // an array that will be populated with substring matches
+                matches = [];
+
+                // regex used to determine if a string contains the substring `q`
+                substrRegex = new RegExp(q, 'i');
+
+                // iterate through the pool of strings and for any string that
+                // contains the substring `q`, add it to the `matches` array
+                $.each(strs, function(i, str) {
+                    if (substrRegex.test(str)) {
+                        matches.push(str);
+                    }
+                });
+
+                cb(matches);
+            };
+        };
+    </script>
 @endsection
