12dbf0640bf55fa78d263f798f43d031178ad3bd
Juan Carlos <juancagb.17@gmail.com>
2016-07-06 09:32:39 -0500

    Search box UI

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,23 @@
 
 @section('title', 'Bienvenido')
 
+@section('title-right')
+    <div class="title_right">
+        <div class="col-md-5 col-sm-5 col-xs-12 form-group pull-right top_search">
+
+            <form class="form-inline">
+                <div class="input-group">
+                    <input type="text" class="form-control" placeholder="Buscar producto ...">
+                    <span class="input-group-btn">
+                        <button class="btn btn-default" type="button">Go</button>
+                    </span>
+                </div>
+            </form>
+
+        </div>
+    </div>
+@endsection
+
 @section('content')
     <div class="row">
         <div class="col-md-12 col-sm-12 col-xs-12">
