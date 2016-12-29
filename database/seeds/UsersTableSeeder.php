713a5702eca1f76789ea4647744e19f426ff65a9
migueljg20 <migueljg_111194@hotmail.com>
2016-12-29 18:55:48 -0500

    Cosas desconocidas

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -5,11 +5,7 @@ use Illuminate\Database\Seeder;
 
 class UsersTableSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
+
     public function run()
     {
         User::create([
